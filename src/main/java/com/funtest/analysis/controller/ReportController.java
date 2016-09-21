9fdb2850ccaeec8b5f9031c555d54a5faca2ae8c
DESKTOP-RQLJO7D\a8518 <851824004@qq.com>
2016-09-21 23:12:48 +0800

    23:12

--- src/main/java/com/funtest/analysis/controller/ReportController.java
+++ src/main/java/com/funtest/analysis/controller/ReportController.java
@@ -1,50 +1,70 @@
 package com.funtest.analysis.controller;
 
-import org.apache.shiro.SecurityUtils;
+import java.io.InputStream;
+
 import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.subject.Subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.funtest.analysis.bean.DataInfo;
+import com.funtest.analysis.bean.Report;
+import com.funtest.analysis.service.ReportService;
 import com.funtest.core.bean.ReturnMsg;
 import com.funtest.core.bean.constant.Constants;
-import com.hexin.dl.util.CustomSessionUtil;
 
 @Controller
-@RequestMapping(value="report")
+@RequestMapping(value = "report")
 public class ReportController {
-	 private  Logger logger = LoggerFactory.getLogger(this.getClass());
-	 
-	    @RequestMapping(value = "creatReport")
-	    @ResponseBody
-	    public Object login(@RequestParam("files") MultipartFile[] files,
-	            @RequestParam(value = "password", required = true) String colUserPwd) {
+	@Autowired
+	ReportService service;
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	        ReturnMsg rm = new ReturnMsg();
-	        Subject curSubject = SecurityUtils.getSubject();
+	@RequestMapping(value = "buildDataInfo")
+	@ResponseBody
+	public Object buildDataInfo(@RequestParam(value="files",required = true) MultipartFile[] files,
+			@RequestParam(value = "reportName", required = true) String reportName,
+			@RequestParam(value = "chipName", required = true) String chipName,
+			@RequestParam(value = "mode", required = false) Integer mode) {
 
-	        try {
-	            UsernamePasswordToken token = new UsernamePasswordToken(colUserName, colUserPwd);
-	            curSubject.login(token);
-
-	            //设置数据权限拦截中的登录用户数据
-	            //            RestrictionParser.setLoginUser(CustomSessionUtil.getLoginUser());
-	           // CustomSessionUtil.cancelUserForcedToLogoutMark();
-
-	            rm.setCode(Constants.RETURN_MSG_SUCCESS);
-	            curSubject.hasRole("dummy");
-	            logger.info("User \"" +CustomSessionUtil.getLoginAdminName()+"\" is log in.");
-	        } catch (AuthenticationException e) {
-	        	logger.error(e.getMessage(), e);
-	            rm.setCode(Constants.RETURN_MSG_FAILURE);
-	            rm.setMessage(Constants.LOGIN_FAILED);
-	        }      
-	        return rm;
-	    }
+		ReturnMsg rm = new ReturnMsg();
+		try {
+			String[] fileNames=new String[files.length];
+			InputStream[] ins=new InputStream[files.length];
+			for(int i=0;i<files.length;i++){
+				fileNames[i]=files[i].getOriginalFilename();
+				ins[i]=files[i].getInputStream();
+			}
+			DataInfo dataInfo = service.createDataInfo(fileNames, ins, reportName, chipName, mode);
+			rm.setData(dataInfo);
+			rm.setCode(Constants.RETURN_MSG_SUCCESS);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			rm.setCode(Constants.RETURN_MSG_FAILURE);
+			rm.setMessage(e.getMessage());
+		}
+		return rm;
+	}
+	
+	@RequestMapping(value="buildReport")
+	@ResponseBody
+	public Object buildReport(@RequestParam(value="dataInfo",required=true)DataInfo dataInfo){
+		ReturnMsg rm = new ReturnMsg();
+		try {
+			Report report = service.createReport(dataInfo);
+			rm.setData(report);
+			rm.setCode(Constants.RETURN_MSG_SUCCESS);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			rm.setCode(Constants.RETURN_MSG_FAILURE);
+			rm.setMessage(e.getMessage());
+		}
+		return rm;
+	}
 }
--- src/main/java/com/funtest/analysis/service/ReportService.java
+++ src/main/java/com/funtest/analysis/service/ReportService.java
@@ -1,5 +1,13 @@
 package com.funtest.analysis.service;
 
-public interface ReportService {
+import java.io.IOException;
+import java.io.InputStream;
 
+import com.funtest.analysis.bean.DataInfo;
+import com.funtest.analysis.bean.Report;
+
+public interface ReportService {
+	public Report createReport(DataInfo dataInfo);
+	public DataInfo createDataInfo(String[] fileNames, InputStream[] ins,String reportName,String chipName,Integer mode) throws IOException;
+		
 }
